ccd815cef4bb0a8479f6f83b66a8afda91b35384
WolfgangMatyus <wi21b055@technikum-wien.at>
2022-12-06 22:33:34 +0100

    navbar

---
+++ Portfolio/elements/nav.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <ul>
+        <div class="col-3 sideMenu">
+            <li><a href="default.asp">Home</a></li>
+        </div>
+        <div class="col-4 sideMenu">
+            <li><a href="news.asp">News</a></li>
+        </div>
+        <div class="col-5 sideMenu">
+            <li><a href="contact.asp">Contact</a></li>
+        </div>
+        <div class="col-6 sideMenu">
+            <li><a href="about.asp">About</a></li>
+        </div>
+    </ul> 
--- Portfolio/index.php
+++ Portfolio/index.php
@@ -1,34 +1,39 @@
 <?php
     include 'elements/head.php';
 ?>
-<?php
-    include 'elements/nav.php';
-?>
+
 
 <div class="header">
   <h1>Wolfgang Matyus</h1>
   <h1>Willkommen zu meinem perslönlichen Portfolio!</h1>
 </div>
+<?php
+    include 'elements/nav.php';
+?>
 <hr>
 <div class="row">
-  <div class="col-3 menu">
+  <div class="col-3 sideMenu">
     <ul class="sidebar">
-        <a href="#persDaten" class="nav-link
+        <li  class="nav-link 
           <?php if(basename($_SERVER['PHP_SELF'])=="index.php#persDaten"){echo 'active';}else{echo '';}//setzt aktiven link active?>
-            "><li>Persönliche Daten</li></a>
-        <a href="#pastJob" class="nav-link
+            "><a href="#persDaten">Persönliche Daten</a></li>
+        <li  class="nav-link 
           <?php if(basename($_SERVER['PHP_SELF'])=="index.php#pastJob"){echo 'active';}else{echo '';}//setzt aktiven link active?>
-            "><li>Beruflicher Werdegang</li></a>
-        <a href="#school" class="nav-link
+            "><a href="#pastJob">Beruflicher Werdegang</a></li>
+        <li  class="nav-link 
           <?php if(basename($_SERVER['PHP_SELF'])=="index.php#school"){echo 'active';}else{echo '';}//setzt aktiven link active?>
-            "><li>Schulische Laufbahn</li></a>
+            "><a href="#school">Schulische Laufbahn</a></li>
     </ul>
   </div>
 
   <div class="col-9">
     <p>
     <?php
-      echo $_SERVER['PHP_SELF'];
+        echo $_SERVER['PHP_SELF'] .'<br>';
+        echo $_SERVER['SCRIPT_FILENAME'] .'<br>';
+        echo $_SERVER['SCRIPT_NAME'] .'<br>';
+        echo $_SERVER['SCRIPT_URI'] .'<br>';
+      
         if(basename($_SERVER['PHP_SELF'])=="index.php#persDaten"){echo '<h1>Perönliche Daten</h1>';}else{echo '';}?>
     </p>
   </div>
