8a0a58cc7b729a649ebe9105ddea71b8714d7071
WolfgangMatyus <wi21b055@technikum-wien.at>
2022-12-06 13:39:23 +0100

    init commit

---
+++ Portfolio/index.php
@@ -0,0 +1,15 @@
+<?php
+    include 'elements/head.php';
+    include 'elements/nav.php';
+?>
+<div>
+    <h1>Wolfgang Matyus</h1>
+    <h1>Willkommen zu meinem perslönlichen Portfolio!</h1>
+</div>
+
+
+<?php
+
+    include 'elements/footer.php';
+
+?>
